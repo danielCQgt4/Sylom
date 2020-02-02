dd68da79805b3f59626a2581b86aa9791a45265a
Daniel Coto Quiros <danielcotoquiros@hotmail.com>
2020-02-01 22:34:21 -0600

    Commit middle way own fw and pages

--- src/SylomSLN/MVC/App_Start/BundleConfig.cs
+++ src/SylomSLN/MVC/App_Start/BundleConfig.cs
@@ -5,23 +5,31 @@ namespace MVC {
     public class BundleConfig {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            #region Scripts
+            bundles.Add(new ScriptBundle("~/bundles/cycScript").Include(
+                        "~/Public/JS/cdGENCY1.js"
+                ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/sripts").Include(
+                        "~/Public/JS/jsFBBw411.slim.min.js",
+                        "~/Public/JS/jsPFBw411.min.js",
+                        "~/Public/JS/jsBBFBw411.min.js"));
+            #endregion
+
+            #region Modernizr
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Public/JS/modernizr-*"));
+            #endregion
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            #region Styles
+            bundles.Add(new ScriptBundle("~/bundles/styles").Include(
+                        "~/Public/CSS/bootstrap.min.css",
+                        "~/Public/CSS/cdGENCY1.css",
+                        "~/Public/CSS/cdGENCY1_img.css",
+                        "~/Public/CSS/cycStylesFW.min.css"
+                        ));
+            #endregion
         }
     }
 }
