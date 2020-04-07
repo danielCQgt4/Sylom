7466b929c92909d6269965e5c812c0382da20fae
Daniel Coto Quiros <danielcotoquiros@hotmail.com>
2020-04-07 00:31:24 -0600

    Commit api after create models

---
+++ src/SylomSLN/API/Models/Seguridad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models {
+    public static class Seguridad
+    {
+
+        /// Encripta una cadena
+        public static string Encriptar(this string _cadenaAencriptar)
+        {
+            string result;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+
+        /// Esta función desencripta la cadena que le envíamos en el parámentro de entrada.
+        public static string DesEncriptar(this string _cadenaAdesencriptar)
+        {
+            string result;
+            byte[] decryted = Convert.FromBase64String(_cadenaAdesencriptar);
+            //result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+            result = System.Text.Encoding.Unicode.GetString(decryted);
+            return result;
+        }
+    }
+}
